261aaf3ef40fad5817a81f359b52ba14b1858a48
ossielromero <ossiel@riseup.net>
2019-05-22 17:16:10 -0500

    first commit
    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAlLCHxzJXnPhfnlGC48TACUKX-uYbfBVs',
+  authDomain: 'Demo-Todo-React-Redux.firebaseapp.com',
+  databaseURL: 'https://Demo-Todo-React-Redux.firebaseio.com',
+  storageBucket: 'Demo-Todo-React-Redux.appspot.com'
+};
